8d2b7f1792b243c39e285f7d7227690a050ea951
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-10 21:09:52 -0300

    Create ex.04.py

---
+++ ex.04.py
@@ -0,0 +1,12 @@
+#4. Quadrado - Escrever um programa que imprima a seguinte figura:
+#XXXXX
+#X   X
+#X   X
+#X   X
+#XXXXX
+
+print ("xxxxx")
+print ("x   x")
+print ("x   x")
+print ("x   x")
+print ("xxxxx")
