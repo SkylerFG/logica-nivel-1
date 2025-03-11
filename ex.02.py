9be42206c4330c79e43758cbae3cb66bd8e50e23
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-10 21:10:36 -0300

    Update ex.02.py

--- ex.02.py
+++ ex.02.py
@@ -1,3 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
 print("Felipe Garcia")
 print("Rio de janeiro")
 print("Magé")
