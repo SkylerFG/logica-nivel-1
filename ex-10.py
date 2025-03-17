55c102179689f485d3151abc578367311a827d10
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-17 19:30:58 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,8 @@
+#escreva um software quw informe a média aritimética de um aluno que cursou 4 bimestres
+
+nota1=int(input("Nota primeiro bimestre: "))
+nota2=int(input("Nota segundo bimestre: "))
+nota3=int(input("Nota terceiro bimestre: "))
+nota4=int(input("Nota quarto bimestre: "))
+media=(nota1+nota2+nota3+nota4)/4
+print("A média do aluno é:", media)
