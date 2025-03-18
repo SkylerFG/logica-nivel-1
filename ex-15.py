c21fdc6cd97fe49921a4dd34a148d015a5d80767
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-17 21:31:28 -0300

    Create ex-15.py

---
+++ ex-15.py
@@ -0,0 +1,7 @@
+# Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo)
+
+valor=int(input("Digite um número: "))
+if valor % 2 == 1:
+    print("O número é ímpar")
+else:
+    print("O número é par")
