84b492e4aa71141cbba1c1a570b73c9701853abc
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-17 21:21:40 -0300

    Create ex-14.py

---
+++ ex-14.py
@@ -0,0 +1,7 @@
+#Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 
+
+idade=int(input("Digite um número: "))
+if idade >=10:
+    print ("MAIOR QUE 10!")
+else:
+    print ("MENOR QUE 10!")
