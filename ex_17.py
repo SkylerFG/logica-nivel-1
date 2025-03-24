e021df90eaec33f643860f3e5590723317bf0edf
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-24 19:09:03 -0300

    Create ex_17.py

---
+++ ex_17.py
@@ -0,0 +1,12 @@
+#Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+
+n1=int(input("Diga o primeiro número: "))
+n2=int(input("Diga o segundo número: "))
+n3=int(input("Diga o terceiro número: "))
+if n1 > n2 >= n3:
+    nm=n1
+elif n2 >= n1 >= n3:
+    nm = n2
+else:
+    nm = n3
+print ("O maior número é o ", nm)
