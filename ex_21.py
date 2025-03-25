557129c13f78cdf1f5e907638e9a3610324af936
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-24 21:08:29 -0300

    Create ex_21.py

---
+++ ex_21.py
@@ -0,0 +1,12 @@
+#Verificação de Nota - Peça ao usuário para digitar sua nota (0 a 10) e classifique:
+#Nota ≥ 7: Aprovado
+#Nota entre 5 e 6.9: Recuperação
+#Nota < 5: Reprovado
+
+nota=float(input("Diga sua idade: "))
+if nota >= 7:
+    print ("Você está aprovado")
+elif nota <= 6.9 and nota > 5:
+    print ("Você está de recuperação")
+else:
+    print ("Você está reprovado")
