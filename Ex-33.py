772ef6f11a3d9b04cfdd7fb33e2a5f7bcc8212d1
SkylerFG <contato.lipegarcia@gmail.com>
2025-06-09 19:07:59 -0300

    Create Ex-33.py

---
+++ Ex-33.py
@@ -0,0 +1,6 @@
+#Evoluir o exercício de sumar 2 números p/ usar função e retornar o valor resultante
+#OBS: Receber os valores por parâmetros
+
+def soma (num1, num2): 
+    return (num1+num2)
+print(soma(6,2))
